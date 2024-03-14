bfffa3c11c09405cb6776dd12ff8e6b38146ed66
KasaganiGanesh <ganeshkasagani@lyrostech.com>
2024-03-14 16:52:59 +0530

    changed

--- src/Webpages.js
+++ src/Webpages.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
   return (
     <div>
-        <a href="https://kasaganiganesh.github.io/app2/">project-1</a>
+        <a href="https://kasaganiganesh.github.io/app2/">project-1</a><br/>
         <a href="https://kasaganiganesh.github.io/ecomm-react/">coffee- shop project</a>
 
     </div>
